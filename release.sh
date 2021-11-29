a4e49ec2b6ca034b1f6de80d1f3b65f26914e677
tanzhiyong <tanzhiyong@dianmi365.com>
2021-11-29 10:46:42 +0800

    #00000 change something

--- release.sh
+++ release.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-#
-# PROJECT[sh-realpath]
-# License: https://github.com/mkropat/sh-realpath/blob/master/LICENSE.txt
-#
 
 
 # 使用说明:
@@ -14,120 +10,14 @@
 # 额外的, 可以通过 -i 指定commit的全hash 作为指定commit 提交
 # eg:  ./release.sh  -i  632e50e36f694f67650350f2f3dc0985cb9142a0 -v 1.0.0 -m "the first release for version 1.0.0"
 
-realpath() {
-    canonicalize_path "$(resolve_symlinks "$1")"
-}
-
-resolve_symlinks() {
-    _resolve_symlinks "$1"
-}
-
-_resolve_symlinks() {
-    _assert_no_path_cycles "$@" || return
-
-    local dir_context path
-    path=$(readlink -- "$1")
-    if [ $? -eq 0 ]; then
-        dir_context=$(dirname -- "$1")
-        _resolve_symlinks "$(_prepend_dir_context_if_necessary "$dir_context" "$path")" "$@"
-    else
-        printf '%s\n' "$1"
-    fi
-}
-
-_prepend_dir_context_if_necessary() {
-    if [ "$1" = . ]; then
-        printf '%s\n' "$2"
-    else
-        _prepend_path_if_relative "$1" "$2"
-    fi
-}
-
-_prepend_path_if_relative() {
-    case "$2" in
-        /* ) printf '%s\n' "$2" ;;
-         * ) printf '%s\n' "$1/$2" ;;
-    esac
-}
-
-_assert_no_path_cycles() {
-    local target path
-
-    target=$1
-    shift
-
-    for path in "$@"; do
-        if [ "$path" = "$target" ]; then
-            return 1
-        fi
-    done
-}
-
-canonicalize_path() {
-    if [ -d "$1" ]; then
-        _canonicalize_dir_path "$1"
-    else
-        _canonicalize_file_path "$1"
-    fi
-}
-
-_canonicalize_dir_path() {
-    (cd "$1" 2>/dev/null && pwd -P)
-}
-
-_canonicalize_file_path() {
-    local dir file
-    dir=$(dirname -- "$1")
-    file=$(basename -- "$1")
-    (cd "$dir" 2>/dev/null && printf '%s/%s\n' "$(pwd -P)" "$file")
-}
-
-# Optionally, you may also want to include:
-
-### readlink emulation ###
-
-readlink() {
-    if _has_command readlink; then
-        _system_readlink "$@"
-    else
-        _emulated_readlink "$@"
-    fi
-}
-
-_has_command() {
-    hash -- "$1" 2>/dev/null
-}
-
-_system_readlink() {
-    command readlink "$@"
-}
-
-_emulated_readlink() {
-    if [ "$1" = -- ]; then
-        shift
-    fi
-
-    _gnu_stat_readlink "$@" || _bsd_stat_readlink "$@"
-}
-
-_gnu_stat_readlink() {
-    local output
-    output=$(stat -c %N -- "$1" 2>/dev/null) &&
-
-    printf '%s\n' "$output" |
-        sed "s/^‘[^’]*’ -> ‘\(.*\)’/\1/
-             s/^'[^']*' -> '\(.*\)'/\1/"
-    # FIXME: handle newlines
-}
-
-_bsd_stat_readlink() {
-    stat -f %Y -- "$1" 2>/dev/null
-}
-
 release_usage()
 {
 	echo "Usage: $0 [ -v release version ] [ -i commit_id, support null ] [ -m release messgae, support null ]" 1>&2
 	exit 1
+}
+branch_support(){
+  the_err_branch=`git rev-parse --abbrev-ref HEAD`
+  echo "Branch Limit: only support main/dev/test as the release branch, ${the_err_branch} not allowed!!!"
 }
 
 COMMIT_BY_SPEC_SHA=
@@ -175,13 +65,10 @@ if [ "${is_git_repo}" = "0" ]; then
 		"dev"):
 			COMMIT_BY_BRANCH=dev
 			;;
-		"develop"):
-			COMMIT_BY_BRANCH=dev
-			;;
 		"test"):
 			COMMIT_BY_BRANCH=test
 			;;
-		"master"):
+		"main"):
 			COMMIT_BY_BRANCH=pd
 			;;
 		*)
